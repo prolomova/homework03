3744eb8b0ee86f5f7e5ee4b79c1e1ee2d8be031b
Yulia <prolomova2012@yandex.ru>
2018-10-13 13:02:43 +0500

    исправлена опечатка

--- src/main/scala/fintech/homework03/PrefixTree.scala
+++ src/main/scala/fintech/homework03/PrefixTree.scala
@@ -13,14 +13,14 @@ trait IPrefixTree[K, +V] {
   def get: V
 }
 
-class PrefixTree[K, +V](val subTree: Map[K, PrefixTree[K, V]], val value: Option[V]) extends IPrefixTree[K, V]{
+class PrefixTree[K, +V](val subTrees: Map[K, PrefixTree[K, V]], val value: Option[V]) extends IPrefixTree[K, V]{
 
-  override def hashCode(): Int = 41 * (41 * value.hashCode()) + subTree.hashCode()
+  override def hashCode(): Int = 41 * (41 * value.hashCode()) + subTrees.hashCode()
 
   override def equals(a: Any): Boolean = {
     a match {
       case prefixTree: PrefixTree[K, V] => prefixTree.value.getOrElse() == value.getOrElse() &&
-        prefixTree.subTree == subTree
+        prefixTree.subTrees == subTrees
       case _ => false
     }
   }
@@ -28,13 +28,13 @@ class PrefixTree[K, +V](val subTree: Map[K, PrefixTree[K, V]], val value: Option
   def put[U >: V](path: Seq[K], value: U): IPrefixTree[K, U] = {
     val curTreeLevel = this
     def addValue(curTreeLevel : PrefixTree[K, U], path: Seq[K]): PrefixTree[K, U] ={
-      if (path.isEmpty) new PrefixTree[K, U](curTreeLevel.subTree, Some(value))
+      if (path.isEmpty) new PrefixTree[K, U](curTreeLevel.subTrees, Some(value))
       else {
         val nextTreeLevel =
-        if (!curTreeLevel.subTree.contains(path.head))
+        if (!curTreeLevel.subTrees.contains(path.head))
           new PrefixTree[K, U](Map.empty, None)
-        else curTreeLevel.subTree(path.head)
-        new PrefixTree[K, U](curTreeLevel.subTree.+(path.head -> addValue(nextTreeLevel, path.tail)),
+        else curTreeLevel.subTrees(path.head)
+        new PrefixTree[K, U](curTreeLevel.subTrees.+(path.head -> addValue(nextTreeLevel, path.tail)),
           curTreeLevel.value)
       }
     }
@@ -44,14 +44,14 @@ class PrefixTree[K, +V](val subTree: Map[K, PrefixTree[K, V]], val value: Option
   def sub(path: Seq[K]): IPrefixTree[K, V] = {
     val curTreeLevel = this
     @tailrec
-    def getSubTree(curTreeLevel : PrefixTree[K, V], path: Seq[K]): IPrefixTree[K, V] ={
+    def getsubTrees(curTreeLevel : PrefixTree[K, V], path: Seq[K]): IPrefixTree[K, V] ={
       if (path.isEmpty) curTreeLevel
       else {
-        if (curTreeLevel.subTree.contains(path.head)) getSubTree(curTreeLevel.subTree(path.head), path.tail)
+        if (curTreeLevel.subTrees.contains(path.head)) getsubTrees(curTreeLevel.subTrees(path.head), path.tail)
         else new PrefixTree[K, V](Map.empty, None)
       }
     }
-    getSubTree(curTreeLevel, path)
+    getsubTrees(curTreeLevel, path)
   }
 
   def get: V = value.get
